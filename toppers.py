cf3b4849508bc1384153628a6f2dd854a2766c25
Harrymentis10 <harrymentis10@gmail.com>
2025-10-06 20:28:30 +1100

    Update toppers.py

--- toppers.py
+++ toppers.py
@@ -1,6 +1,8 @@
 import swift
 from spatialgeometry import Mesh
 from spatialmath import SE3
+from spatialmath.base import *
+from math import pi
 
 class topperPython:
     def __init__(self, filename, startPos, endPos, env: 'swift.Swift'):
@@ -8,7 +10,8 @@ class topperPython:
         self.endPos = endPos
         self.file = filename
         self.env = env
-        self.offset = SE3(-0.07, 0, 0.143).A
+        self.offset = SE3(-0.07, 0, 0.143).A @ trotx(pi)
+        self.ttoffset = SE3(0.0, 0, 0.04).A @ trotx(pi)
 
         self.meshObj = Mesh(filename = self.file)
         self.meshObj.T = self.startPos
