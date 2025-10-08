a556171ff85f86a3192b3c9d8cbaa48f0520a82a
Harrymentis10 <harrymentis10@gmail.com>
2025-10-08 17:09:03 +1100

    Update main.py

--- main.py
+++ main.py
@@ -33,10 +33,16 @@ def initialise():
     # Add UR3 and Move to starting position
     ur3.base = SE3(1.5, 4.0, 0.585).A 
     ur3.add_to_env(env)
+    env.add(ur3.dhRobot)
+    ur3.dhRobot.base = SE3(1.5, 4.0, 0.585).A 
+    ur3.dhRobot.q = ur3.q
 
     # Add GP4 and Move to starting position
     gp4.base = SE3(1.5, 3.1, 0.585).A @ trotz(pi/2)
     gp4.add_to_env(env)
+    env.add(gp4.dhRobot)
+    gp4.dhRobot.base = SE3(1.5, 3.1, 0.585).A @ trotz(pi/2)
+    gp4.dhRobot.q = gp4.q
 
     # Add centrifuge base and move to starting position
     cenBaseFile = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/CentrifugeBottom.dae'
@@ -108,81 +114,9 @@ def initialise():
 
     botSystem.simulation()
 
-    #rmrc(gp4, topperOffset, eeOffset)
+    #rmrc(gp4, topperEEOffset, SE3(0.07, 0, 0).A)
 
-def rmrc(robot: 'newGP4', offsetTop: 'SE3', eeOffset: 'SE3'):
-    # 1.1) Set parameters for the simulation                 
-    t = 10                                     # Total time (s)
-    delta_t = 0.02                             # Control frequency
-    steps = int(t/delta_t)                     # No. of steps for simulation
-    delta = 2*pi/steps                         # Small angle change
-    epsilon = 0.1                              # Threshold value for manipulability/Damped Least Squares
-    W = np.diag([1, 1, 1, 0, 0, 0])      # Weighting matrix for the velocity vector
 
-    # 1.2) Allocate array data
-    m = np.zeros([steps,1])                    # Array for Measure of Manipulability
-    q_matrix = np.zeros([steps,6])             # Array for joint anglesR
-    qdot = np.zeros([steps,6])                 # Array for joint velocities
-    theta = np.zeros([3,steps])                # Array for roll-pitch-yaw angles
-    x = np.zeros([3,steps])                    # Array for x-y-z trajectory
-    position_error = np.zeros([3,steps])       # For plotting trajectory error
-    angle_error = np.zeros([3,steps])          # For plotting trajectory error
-
-    # 1.3) Set up trajectory, initial pose
-    s = trapezoidal(0,1,steps).q
-    currentPos = gp4.fkine(gp4.q).A @ eeOffset             # Trapezoidal trajectory scalar
-    for i in range(steps):
-        x[0,i] = currentPos[0,3]   # Points in x
-        x[1,i] = currentPos[1,3]                # Points in y
-        x[2,i] = currentPos[2,3] - 0.05 * s[i]               # Points in z
-        theta[0,i] = 0                          # Roll angle 
-        theta[1,i] = 0                          # Pitch angle
-        theta[2,i] = 0                          # Yaw angle
-    
-    T = transl(x[:,0]) @ rpy2tr(theta[0,0], theta[1,0], theta[2,0])       # Create transformation of first point and angle     
-    q0 = np.zeros([1,6])                                                  # Initial guess for joint angles
-    q_matrix[0,:] = gp4.ikine_LM(T, q0).q                                # Solve joint angles to achieve first waypoint
-    qlim = np.transpose(gp4.qlim)
-    
-    # 1.4) Track the trajectory with RMRC
-    for i in range(steps-1):
-        T = gp4.fkine(q_matrix[i,:]).A                                   # Get forward transformation at current joint state
-        delta_x = x[:,i+1] - T[:3,3]                                      # Get position error from next waypoint
-        Rd = rpy2r(theta[0,i+1], theta[1,i+1], theta[2,i+1])              # Get next RPY angles, convert to rotation matrix
-        Ra = T[:3,:3]                                                     # Current end-effector rotation matrix
-        Rdot = (1/delta_t)*(Rd - Ra)                                      # Calculate rotation matrix error
-        S = Rdot @ Ra.T                                                   # Skew symmetric!
-        linear_velocity = (1/delta_t)*delta_x
-        angular_velocity = np.array([S[2,1], S[0,2], S[1,0]])             # Check the structure of Skew Symmetric matrix!!
-        delta_theta = tr2rpy(Rd @ Ra.T, order='xyz')                      # Convert rotation matrix to RPY angles
-        xdot = W @ np.vstack((linear_velocity.reshape(3,1), 
-                              angular_velocity.reshape(3,1)))             # Calculate end-effector velocity to reach next waypoint.
-        J = gp4.jacob0(q_matrix[i,:])                                    # Get Jacobian at current joint state
-        m[i] = np.sqrt(np.linalg.det(J @ J.T))
-        if m[i] < epsilon:                                                # If manipulability is less than given threshold
-            m_lambda = (1 - m[i]/epsilon) * 0.05
-            print("SINUGLARITY")
-        else:
-            m_lambda = 0
-        inv_j = np.linalg.inv(J.T @ J + m_lambda * np.eye(6)) @ J.T          # DLS Inverse
-        qdot[i,:] = (inv_j @ xdot).T                                      # Solve the RMRC equation (you may need to transpose the         vector)
-        for j in range(6):                                                # Loop through joints 1 to 6
-            if q_matrix[i,j] + delta_t*qdot[i,j] < qlim[j,0]:             # If next joint angle is lower than joint limit...
-                qdot[i,j] = 0 # Stop the motor
-                print("LOWER")
-            elif q_matrix[i,j] + delta_t*qdot[i,j] > qlim[j,1]:           # If next joint angle is greater than joint limit ...
-                qdot[i,j] = 0 # Stop the motor
-                print("HIGHER")
-            
-        q_matrix[i+1,:] = q_matrix[i,:] + delta_t*qdot[i,:]               # Update next joint state based on joint velocities
-        
-        
-        position_error[:,i] = x[:,i+1] - T[:3,3]                          # For plotting
-        angle_error[:,i] = delta_theta                                    # For plotting
-
-    # 1.5) Plot the results
-    for q in q_matrix:
-        gp4.q = q
 # ---------------------------------------------------------------------------------------#
 if __name__ == "__main__": 
     #Launch swift   
@@ -203,4 +137,5 @@ if __name__ == "__main__":
 
     env.step(1)
 
+    print("FINISHED")
     env.hold()
