daa435f73961f9f3b8a2fec5c82045e44de434b9
Harrymentis10 <harrymentis10@gmail.com>
2025-10-11 13:20:53 +1100

    Update main.py

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ from tkinterGUI import JointControlUI
 def initialise():
     # Add room
     #A2-GithunTotalCode-8-10-25\IRAssesement2-main\enivornmentFiles\CentrifugeEnvironment.dae
-    #envFile = r'enivornmentFiles\CentrifugeEnvironment.dae' # FOR JAYDEN
-    envFile = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/CentrifugeEnvironment.dae' # FOR HARRY
+    envFile = r'enivornmentFiles\CentrifugeEnvironment.dae' # FOR JAYDEN
+    #envFile = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/CentrifugeEnvironment.dae' # FOR HARRY
     envRoom = Mesh(filename = envFile)
     env.add(envRoom)
 
@@ -64,48 +64,48 @@ def initialise():
     
 
     # Add centrifuge base and move to starting position
-    #cenBaseFile = r'enivornmentFiles\CentrifugeBottom.dae'
-    cenBaseFile = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/CentrifugeBottom.dae' # FOR HARRY'
+    cenBaseFile = r'enivornmentFiles\CentrifugeBottom.dae'
+    #cenBaseFile = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/CentrifugeBottom.dae' # FOR HARRY'
     cenBase = Mesh(filename = cenBaseFile)
     cenBase.T = SE3(1.8, 4.2, 0.68).A #1.1, 4.2
     env.add(cenBase)
 
     # Add centrifuge top and move to starting position
-    #cenTopFile = r'enivornmentFiles\CentrifugeTop.dae' # FOR JAYDEN
-    cenTopFile = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/CentrifugeTop.dae' # FOR HARRY
+    cenTopFile = r'enivornmentFiles\CentrifugeTop.dae' # FOR JAYDEN
+    #cenTopFile = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/CentrifugeTop.dae' # FOR HARRY
     cenTop = Mesh(filename = cenTopFile)
     cenTop.T = cenBase.T @ SE3(-0.003, 0.138, 0.0372).A @ trotx(-4*pi/5) #0.46
     env.add(cenTop)
 
     # Add test tube Rack and Move to starting position
-    #testTubeRackFile = r'enivornmentFiles\TesttubeRack.dae' # FOR JAYDEN
-    testTubeRackFile = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/TesttubeRack.dae' # FOR HARRY
+    testTubeRackFile = r'enivornmentFiles\TesttubeRack.dae' # FOR JAYDEN
+    #testTubeRackFile = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/TesttubeRack.dae' # FOR HARRY
     testTubeRack = Mesh(filename = testTubeRackFile)
     testTubeRack.T = SE3(1.51, 3.55, 0.585).A @ trotz(pi/2) #1.36
     env.add(testTubeRack)
 
     # Add specimens to extract liquid from 
     specimen1File = r'enivornmentFiles\Specimen1.dae' # FOR JAYDEN
-    specimen1File = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/specimen1.dae' # FOR HARRY
+    #specimen1File = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/specimen1.dae' # FOR HARRY
     specimen1 = Mesh(filename = specimen1File)
     specimen1.T = SE3(1.0, 3.1, 0.685).A @ trotz(pi/2) #1.36
     env.add(specimen1)
 
-    #specimen2File = r'enivornmentFiles\Specimen2.dae' # FOR JAYDEN
-    specimen2File = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/specimen2.dae' # FOR HARRY
+    specimen2File = r'enivornmentFiles\Specimen2.dae' # FOR JAYDEN
+    #specimen2File = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/specimen2.dae' # FOR HARRY
     specimen2 = Mesh(filename = specimen2File)
     specimen2.T = SE3(1.0, 3.9, 0.685).A @ trotz(pi/2) #1.36
     env.add(specimen2)
 
-    #topperEEFile = r'enivornmentFiles\topperEE.dae' # FOR JAYDEN
-    topperEEFile = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/topperEE.dae' # FOR HARRY
+    topperEEFile = r'enivornmentFiles\topperEE.dae' # FOR JAYDEN
+    #topperEEFile = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/topperEE.dae' # FOR HARRY
     topperEE = Mesh(filename = topperEEFile)
     topperEEOffset = trotx(pi) @ SE3(-0.07, 0, 0).A
     topperEE.T = gp4.fkine(gp4.q).A @ topperEEOffset
     env.add(topperEE)
 
-    #topperFile = r'enivornmentFiles\Topper.dae' # FOR JAYDEN
-    topperFile = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/Topper.dae' # FOR HARRY
+    topperFile = r'enivornmentFiles\Topper.dae' # FOR JAYDEN
+    #topperFile = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/Topper.dae' # FOR HARRY
     topPlanePoint = SE3(1.92, 2.9, 0.6).A
     topyOff = SE3(0, 0.05, 0).A
 
@@ -118,8 +118,8 @@ def initialise():
 
 
     #Rebel gripper 
-    #pippetteEEFile = r'pippette.dae' # FOR JAYDEN
-    pippetteEEFile = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/pippette.dae' # FOR HARRY
+    pippetteEEFile = r'pippette.dae' # FOR JAYDEN
+    #pippetteEEFile = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/pippette.dae' # FOR HARRY
     pipetteEE = Mesh(filename = pippetteEEFile)
     pipetteEEOffset = SE3(0, 0, 0).A
     pipetteEE.T = rebel.fkine(rebel.q).A @ troty(pi/2)  @ pipetteEEOffset 
@@ -138,8 +138,8 @@ def initialise():
     env.set_camera_pose(position=camera_position, look_at = ur3.base.A[:3, 3])
 
     # Add Test Tubes to Rack
-    #testTubeFileName = r'enivornmentFiles\TestTube.dae' # FOR JAYDEN
-    testTubeFileName = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/TestTube.dae' # FOR HARRY
+    testTubeFileName = r'enivornmentFiles\TestTube.dae' # FOR JAYDEN
+    #testTubeFileName = '/Users/harrymentis/Documents/SensorsAndControls/Assignment2/environmentFiles/TestTube.dae' # FOR HARRY
     tt1 = testTubePython(testTubeFileName, SE3(testTubeRack.T[0, 3] + 0.075, 3.548, 0.65).A, SE3(1.8, 4.13, 0.68).A, env)
     tt2 = testTubePython(testTubeFileName, SE3(testTubeRack.T[0, 3] + 0.029, 3.548, 0.65).A, SE3(1.736, 4.175, 0.68).A, env)
     tt3 = testTubePython(testTubeFileName, SE3(testTubeRack.T[0, 3] - 0.018, 3.548, 0.65).A, SE3(1.866, 4.172, 0.68).A, env)
